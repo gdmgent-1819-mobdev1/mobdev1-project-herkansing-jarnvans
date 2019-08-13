5f7b3ed179b3d25f35daca479b3b0b3f95cc6dd9
jarnvans <jarnvans@student.arteveldehs.be>
2019-08-13 10:49:57 +0200

    changes

--- src/pages/rooms.js
+++ src/pages/rooms.js
@@ -5,6 +5,12 @@ import functions from '../helpers/functions';
 // Import the template to use
 const dormsTemplate = require('../templates/rooms.handlebars');
 
+const showSearchOption = (showElement, firstHideElement, secondHideElement) => {
+  document.querySelector(showElement).classList.remove('hidden');
+  document.querySelector(firstHideElement).classList.add('hidden');
+  document.querySelector(secondHideElement).classList.add('hidden');
+};
+
 export default () => {
   const user = localStorage.getItem('currentUserId');
   const student = functions.getUserType();
@@ -21,5 +27,17 @@ export default () => {
       document.querySelector('.menu-open').addEventListener('click', functions.openMenu, false);
       document.querySelector('.menu-close').addEventListener('click', functions.closeMenu, false);
       document.querySelector('.log-out').addEventListener('click', functions.signOut, false);
+      document.querySelector('.list-link').addEventListener('click', (e) => {
+        e.preventDefault();
+        showSearchOption('.list', '.map', '.tinder');
+      }, false);
+      document.querySelector('.map-link').addEventListener('click', (e) => {
+        e.preventDefault();
+        showSearchOption('.map', '.list', '.tinder');
+      }, false);
+      document.querySelector('.tinder-link').addEventListener('click', (e) => {
+        e.preventDefault();
+        showSearchOption('.tinder', '.map', '.list');
+      }, false);
     });
 };
